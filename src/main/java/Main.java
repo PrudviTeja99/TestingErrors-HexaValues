f672b3970f1b4dbc7a3b82dbc56ca5a91c754a7a
769901 <prudvitejabandam987@gmail.com>
2022-11-03 10:56:18 +0530

    first msg

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args) {
+        short s=0;
+        int x=07;
+        int y=077;
+        int z=123456;
+        s+=z;
+        System.out.println(""+x+y+z);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(s);
+    }
+}
